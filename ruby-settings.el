5e1573f3e17a01f7234ccc240c9fe171b7b10612
Habibullah Pagarkar <habibpagarkar@gmail.com>
2011-11-22 04:13:15 +0530

    More fiddling with ruby-settings

--- ruby-settings.el
+++ ruby-settings.el
@@ -5,14 +5,32 @@
 (require 'rvm)
 (rvm-use-default)
 
+(eval-after-load 'ruby-mode
+  '(progn
+     ;; work around possible elpa bug
+     (ignore-errors (require 'ruby-compilation))
+     (setq ruby-use-encoding-map nil)
+     (require 'inf-ruby)
+     (add-hook 'ruby-mode-hook 'inf-ruby-keys)
+     (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
+     (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)
+     (define-key ruby-mode-map (kbd "C-c l") "lambda")
+     (define-key ruby-mode-map (kbd "#") 'ruby-interpolate)
+     (require 'ruby-block)
+     (ruby-block-mode t)))
+
+(global-set-key (kbd "C-h r") 'ri)
+
 ;; Rake files are ruby, too, as are gemspecs, rackup files, etc.
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+
+(add-to-list 'completion-ignored-extensions ".rbc")
 
 (defun ruby-interpolate ()
   "In a double quoted string, interpolate."
@@ -24,60 +42,60 @@
     (insert "{}")
     (backward-char 1)))
 
-;; (defun create-project-tags ()
-;;   "Create tags file."
-;;   (interactive)
-;;   (shell-command
-;;    (format "%s -f %s/TAGS -e -R %s" path-to-ctags (textmate-project-root) (directory-file-name (textmate-project-root)))))
+(defun pcomplete/rake ()
+  "Completion rules for the `ssh' command."
+  (pcomplete-here (pcmpl-rake-tasks)))
 
-;; Ruby mode settings
-(add-hook 'ruby-mode-hook
-	  (lambda()
-	    (add-hook 'local-write-file-hooks
-		      '(lambda()
-			 (save-excursion
-			   (untabify (point-min) (point-max))
-			   (delete-trailing-whitespace))))
-	    (set (make-local-variable 'indent-tabs-mode) 'nil)
-	    (set (make-local-variable 'tab-width) 2)
-	    (imenu-add-to-menubar "IMENU")
-	    (setq ruby-deep-indent-paren nil)
-	    (setq c-tab-always-indent nil)
-	    (setq ruby-deep-arglist t)
-	    (require 'ruby-block)
-	    (ruby-block-mode t)
-	    (require 'inf-ruby)
-;;	    (require 'ruby-compilation)
-	    (define-key ruby-mode-map "\C-m" 'newline-and-indent)
-;;	    (unless (file-exists-p (concat (textmate-project-root) "/TAGS"))
-;;	      (create-project-tags))
-))
+(defun pcmpl-rake-tasks ()
+   "Return a list of all the rake tasks defined in the current
+projects.  I know this is a hack to put all the logic in the
+exec-to-string command, but it works and seems fast"
+   (delq nil (mapcar '(lambda(line)
+                        (if (string-match "rake \\([^ ]+\\)" line) (match-string 1 line)))
+                     (split-string (shell-command-to-string "rake -T") "[\n]"))))
 
-;; ;; Install mode-compile to give friendlier compiling support!
-;; (require 'mode-compile)
-;; (require 'mode-compile-kill)
-;; (autoload 'mode-compile "mode-compile"
-;;   "Command to compile current buffer file based on the major mode" t)
-;; (global-set-key "\C-cc" 'mode-compile)
-;; (autoload 'mode-compile-kill "mode-compile"
-;;   "Command to kill a compilation launched by `mode-compile'" t)
-;; (global-set-key "\C-ck" 'mode-compile-kill)
+(defun rake (task)
+  (interactive (list (completing-read "Rake (default: default): "
+                                      (pcmpl-rake-tasks))))
+  (shell-command-to-string (concat "rake " (if (= 0 (length task)) "default" task))))
 
-;; ;;; FIX ME
-;; ;;Rspec mode
-;; ;(require 'rspec-mode)
-;; ;(setq rspec-use-rvm t)
+(eval-after-load 'ruby-compilation
+  '(progn
+     (defadvice ruby-do-run-w/compilation (before kill-buffer (name cmdlist))
+       (let ((comp-buffer-name (format "*%s*" name)))
+         (when (get-buffer comp-buffer-name)
+           (with-current-buffer comp-buffer-name
+             (delete-region (point-min) (point-max))))))
+     (ad-activate 'ruby-do-run-w/compilation)))
 
-;; ;;; FIX ME
-;; ;; Shoulda mode
-;; ;(require 'shoulda-mode)
+(eval-after-load 'ruby-mode
+  '(progn
+     (require 'flymake)
 
-;; ;; rhtml mode
-;; (require 'rhtml-mode)
-;; ; put rhtml templates into rhtml-mode
-;; (setq auto-mode-alist  (cons '("\\.erb$" . rhtml-mode) auto-mode-alist))
-;; ; put any rjs scripts into ruby-mode, as they are basically ruby
-;; (setq auto-mode-alist  (cons '("\\.rjs$" . ruby-mode) auto-mode-alist))
+     ;; Invoke ruby with '-c' to get syntax checking
+     (defun flymake-ruby-init ()
+       (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                          'flymake-create-temp-inplace))
+              (local-file (file-relative-name
+                           temp-file
+                           (file-name-directory buffer-file-name))))
+         (list "ruby" (list "-c" local-file))))
+
+     (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+     (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+
+     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
+           flymake-err-line-patterns)
+
+     (add-hook 'ruby-mode-hook
+               (lambda ()
+                 (when (and buffer-file-name
+                            (file-writable-p
+                             (file-name-directory buffer-file-name))
+                            (file-writable-p buffer-file-name))
+                   (local-set-key (kbd "C-c d")
+                                  'flymake-display-err-menu-for-current-line)
+                   (flymake-mode t))))))
 
 ;; ;; Rinari mode
 ;; (require 'rinari)
@@ -86,6 +104,22 @@
 ;;       (list 'mumamo-after-change-major-mode-hook 'dired-mode-hook 'ruby-mode-hook
 ;;             'css-mode-hook 'yaml-mode-hook 'javascript-mode-hook))
 
+;; ;; rhtml mode
+;; (require 'rhtml-mode)
+;; ; put rhtml templates into rhtml-mode
+;; (setq auto-mode-alist  (cons '("\\.erb$" . rhtml-mode) auto-mode-alist))
+;; ; put any rjs scripts into ruby-mode, as they are basically ruby
+;; (setq auto-mode-alist  (cons '("\\.rjs$" . ruby-mode) auto-mode-alist))
+
+;; ;; FIX ME
+;; ;; Rspec mode
+;; (require 'rspec-mode)
+;; (setq rspec-use-rvm t)
+
+;; ;; FIX ME
+;; ;; Shoulda mode
+;; (require 'shoulda-mode)
+
 ;; ;; Cucumber feature
 ;; ;; language if .feature doesn't have "# language: fi"
 ;; (setq feature-default-language "en")
@@ -93,18 +127,52 @@
 ;; (require 'feature-mode)
 ;; (yas/load-directory "~/.emacs.d/packages/feature-mode/snippets")
 
- (eval-after-load 'ruby-mode
-   '(progn
-      (define-key ruby-mode-map (kbd "#") 'ruby-interpolate)))
-
 ;; ;; RSense
 ;; (setq rsense-home (expand-file-name "~/opt/rsense-0.3"))
 ;; (add-to-list 'load-path (concat rsense-home "/etc"))
 ;; (require 'rsense)
 
+;; ;; (add-hook 'ruby-mode-hook
+;; ;;           (lambda ()
+;; ;;             (add-to-list 'ac-sources 'ac-source-rsense-method)
+;; ;;             (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+
+;; Move to ack-grep mode that you'll create
+;; (defun create-project-tags ()
+;;   "Create tags file."
+;;   (interactive)
+;;   (shell-command
+;;    (format "%s -f %s/TAGS -e -R %s" path-to-ctags (textmate-project-root) (directory-file-name (textmate-project-root)))))
+
+;;;;;;;;;;;;;;;;;;
+
+;; ;; Ruby mode settings
 ;; (add-hook 'ruby-mode-hook
-;;           (lambda ()
-;;             (add-to-list 'ac-sources 'ac-source-rsense-method)
-;;             (add-to-list 'ac-sources 'ac-source-rsense-constant)))
+;; 	  (lambda()
+;; 	    (add-hook 'local-write-file-hooks
+;; 		      '(lambda()
+;; 			 (save-excursion
+;; 			   (untabify (point-min) (point-max))
+;; 			   (delete-trailing-whitespace))))
+;; 	    (set (make-local-variable 'indent-tabs-mode) 'nil)
+;; 	    (set (make-local-variable 'tab-width) 2)
+;; 	    (imenu-add-to-menubar "IMENU")
+;; 	    (setq ruby-deep-indent-paren nil)
+;; 	    (setq c-tab-always-indent nil)
+;; 	    (setq ruby-deep-arglist t)
+;; 	    (define-key ruby-mode-map "\C-m" 'newline-and-indent)
+;; ;;	    (unless (file-exists-p (concat (textmate-project-root) "/TAGS"))
+;; ;;	      (create-project-tags))
+;; ))
+
+;; ;; ;; Install mode-compile to give friendlier compiling support!
+;; ;; (require 'mode-compile)
+;; ;; (require 'mode-compile-kill)
+;; ;; (autoload 'mode-compile "mode-compile"
+;; ;;   "Command to compile current buffer file based on the major mode" t)
+;; ;; (global-set-key "\C-cc" 'mode-compile)
+;; ;; (autoload 'mode-compile-kill "mode-compile"
+;; ;;   "Command to kill a compilation launched by `mode-compile'" t)
+;; ;; (global-set-key "\C-ck" 'mode-compile-kill)
 
 (provide 'ruby-settings)
