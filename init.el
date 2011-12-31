aa888cfec927ba1fa69f6c246a62b2d6e3ec25dd
Habibullah Pagarkar <habib@jhu.edu>
2011-12-31 22:29:33 +0530

    Added Tango Dark theme as default

--- init.el
+++ init.el
@@ -136,6 +136,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes 'tango-dark)
  '(transient-mark-mode t)
  '(truncate-lines t)
  '(auto-compression-mode t)
