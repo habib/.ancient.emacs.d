03c06291b9cfa109ae19f41806d38ec922c53749
Habibullah Pagarkar <habibpagarkar@gmail.com>
2013-05-18 22:03:12 -0500

    Added powerline

--- init.el
+++ init.el
@@ -139,6 +139,10 @@
 (require 'volatile-highlights)
 (volatile-highlights-mode t)
 
+;; Customize powerline
+(require 'powerline)
+(powerline-default-theme)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
