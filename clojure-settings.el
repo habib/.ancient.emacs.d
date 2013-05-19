a882272cc0c1389a38b9aab340b1514c1cd1e916
Habibullah Pagarkar <habibpagarkar@gmail.com>
2013-05-19 01:08:56 -0500

    Added ac-nrepl and updated clojure mode settings

--- clojure-settings.el
+++ clojure-settings.el
@@ -7,12 +7,24 @@
 (require 'nrepl)
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 (add-to-list 'same-window-buffer-names "*nrepl*")
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-popup-stacktraces-in-repl t)
+(setq nrepl-history-file "~/.emacs.d/nrepl-history")
 (add-hook 'nrepl-mode-hook 'subword-mode)
 (add-hook 'nrepl-mode-hook 'paredit-mode)
 
 (eval-after-load 'nrepl
   '(progn
+     (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
      (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+     (nrepl-enable-on-existing-clojure-buffers)
      (subword-mode +1)))
+
+;; Auto completion for NREPL
+(require 'ac-nrepl)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'nrepl-mode))
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 
 (provide 'clojure-settings)
