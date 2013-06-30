55a7d316ca32983281a55062a4d2203b05ac41fd
Habibullah Pagarkar <habibpagarkar@gmail.com>
2013-06-30 12:25:48 -0500

    Previous function call was deprecated

--- ruby-settings.el
+++ ruby-settings.el
@@ -158,7 +158,7 @@ exec-to-string command, but it works and seems fast"
 (setq feature-default-language "en")
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 (require 'feature-mode)
-(yas/load-directory (concat user-emacs-directory
+(yas-load-directory (concat user-emacs-directory
                             "packages/feature-mode/snippets"))
 
 ;; Rinari mode
