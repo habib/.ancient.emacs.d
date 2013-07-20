b602663cfbaaf0ed7e78c72127f5eccb9799049b
Habibullah Pagarkar <habibpagarkar@gmail.com>
2013-07-20 14:23:26 -0500

    Added nose and virtualenv

--- python-settings.el
+++ python-settings.el
@@ -1,12 +1,18 @@
 (setq py-install-directory "~/.emacs.d/packages/python-mode/")
+
+(setq py-load-pymacs-p nil)
 (require 'python-mode)
 (require 'python-pep8)
 (require 'python-pylint)
+
+(require 'nose)
 
 ;; Use IPython
 (setq-default py-shell-name "ipython")
 (setq-default py-which-bufname "IPython")
 (setq py-force-py-shell-name-p t)
+
+;(setq ac-sources '(ac-source-pycomplete))
 
 ;; Switch to the interpreter after executing code
 (setq py-shell-switch-buffers-on-execute-p t)
