f26c2305820814f92f8d4933ac9ed991e8bfe7c5
Habibullah Pagarkar <habibpagarkar@gmail.com>
2013-07-23 06:50:38 -0500

    Added jedi mode that needs ctable and epc. Pip install jedi and epc separately. These give function header completions using autocomplete

--- python-settings.el
+++ python-settings.el
@@ -1,5 +1,11 @@
 (require 'python-pep8)
 (require 'python-pylint)
 (require 'nose)
+(require 'deferred)
+(require 'epc)
+
+(setq jedi:setup-keys t)
+(autoload 'jedi:setup "jedi" nil t)
+(add-hook 'python-mode-hook 'jedi:setup)
 
 (provide 'python-settings)
